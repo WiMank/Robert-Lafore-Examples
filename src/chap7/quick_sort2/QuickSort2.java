0424bd1e8e7d2656927bb454c930b730e0ded72f
Ivan Tananaev <ivan.tananaev@gmail.com>
2019-10-17 14:15:47 +0300

    added QuickSort2

---
+++ src/chap7/quick_sort2/QuickSort2.java
@@ -0,0 +1,125 @@
+package chap7.quick_sort2;
+
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class QuickSort2 extends Applet implements Runnable, ActionListener {
+    private Image offscreenImage;
+    private Graphics offscreenGraphics;
+    private int aWidth;
+    private int aHeight;
+    private Thread runner;
+    private int groupSize = 12;
+    private personGroup thePersonGroup;
+    private boolean runFlag;
+    private int order = 1;
+    private Button newButton;
+    private Button sizeButton;
+    private Button drawButton;
+    private Button runButton;
+    private Button stepButton;
+
+    public void init() {
+        this.thePersonGroup = new personGroup(this.groupSize, this.order);
+        this.setLayout(new FlowLayout(2));
+        this.newButton = new Button("New");
+        this.add(this.newButton);
+        this.newButton.addActionListener(this);
+        this.sizeButton = new Button("Size");
+        this.add(this.sizeButton);
+        this.sizeButton.addActionListener(this);
+        this.drawButton = new Button("Draw");
+        this.add(this.drawButton);
+        this.drawButton.addActionListener(this);
+        this.runButton = new Button("Run");
+        this.add(this.runButton);
+        this.runButton.addActionListener(this);
+        this.stepButton = new Button("Step");
+        this.add(this.stepButton);
+        this.stepButton.addActionListener(this);
+        this.aWidth = this.thePersonGroup.getAppletWidth();
+        this.aHeight = this.thePersonGroup.getAppletHeight();
+        this.offscreenImage = this.createImage(this.aWidth, this.aHeight);
+        this.offscreenGraphics = this.offscreenImage.getGraphics();
+        this.runFlag = false;
+        this.thePersonGroup.setDrawMode(2);
+    }
+
+    public void paint(Graphics var1) {
+        this.thePersonGroup.draw(this.offscreenGraphics);
+        var1.drawImage(this.offscreenImage, 0, 0, this);
+    }
+
+    public void update(Graphics var1) {
+        this.paint(var1);
+    }
+
+    public void actionPerformed(ActionEvent var1) {
+        if (var1.getSource() == this.newButton) {
+            this.runFlag = false;
+            this.order = this.order == 1 ? 2 : 1;
+            this.thePersonGroup = new personGroup(this.groupSize, this.order);
+        } else if (var1.getSource() == this.sizeButton) {
+            this.runFlag = false;
+            this.groupSize = this.groupSize == 12 ? 100 : 12;
+            this.thePersonGroup = new personGroup(this.groupSize, this.order);
+        } else if (var1.getSource() == this.drawButton) {
+            this.runFlag = false;
+            this.thePersonGroup.setDrawMode(2);
+        } else if (var1.getSource() == this.runButton) {
+            this.thePersonGroup.setDrawMode(1);
+            this.runFlag = true;
+        } else if (var1.getSource() == this.stepButton && !this.thePersonGroup.getDone()) {
+            this.runFlag = false;
+            this.thePersonGroup.sortStep();
+            this.thePersonGroup.setDrawMode(1);
+        }
+
+        this.repaint();
+    }
+
+    public void start() {
+        if (this.runner == null) {
+            this.runner = new Thread(this);
+            this.runner.start();
+        }
+
+    }
+
+    public void stop() {
+        this.runner = null;
+    }
+
+    public void run() {
+        Thread var1 = Thread.currentThread();
+
+        while(this.runner == var1) {
+            if (this.runFlag && !this.thePersonGroup.getDone()) {
+                this.thePersonGroup.sortStep();
+                this.repaint();
+                this.thePersonGroup.setDrawMode(1);
+                int var2 = this.groupSize == 12 ? 250 : 75;
+
+                try {
+                    Thread.sleep((long)var2);
+                } catch (InterruptedException var3) {
+                }
+            }
+        }
+
+    }
+
+    public QuickSort2() {
+    }
+}
+
---
+++ src/chap7/quick_sort2/person.java
@@ -0,0 +1,21 @@
+package chap7.quick_sort2;
+
+import java.awt.Color;
+
+class person {
+    private int height;
+    private Color color;
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public person(int var1, Color var2) {
+        this.height = var1;
+        this.color = var2;
+    }
+}
